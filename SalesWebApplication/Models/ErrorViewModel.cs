7ae69bc7c78785fb21c77034caa2bea752c93150
Conrado Alexsander <conradoalexsander@hotmail.com>
2019-12-03 00:21:11 -0300

    Add project files.

---
+++ SalesWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
